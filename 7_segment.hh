de59597e0435eb2c321772461623eb05658dd1f2
zacrogers <38870655+zacrogers@users.noreply.github.com>
2020-01-03 14:01:12 +1300

    Create 7_segment.hh

---
+++ 7_segment.hh
@@ -0,0 +1,18 @@
+#ifndef 7_SEGMENT_H
+#define 7_SEGMENT_H
+
+const uint8_t DIGITS[10] = {0x01};
+
+class 7Segment
+{
+  public:
+    7Segment(uint8_t data, uint8_t clk, uint8_t latch)
+    {
+    
+    }
+  
+  private:
+
+};
+
+#endif /* 7_SEGMENT_H */
