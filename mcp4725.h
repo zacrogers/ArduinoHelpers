a2e87697119741e3faada8a0584a7361a9f5ff97
zacrogers <38870655+zacrogers@users.noreply.github.com>
2020-01-15 11:21:09 +1300

    Create mcp4725.h

---
+++ mcp4725.h
@@ -0,0 +1,28 @@
+/*
+ * mcp4725.h
+ *
+ * Created: 15-Jan-20 9:49:49 AM
+ *  Author: Zac
+ */ 
+
+
+#ifndef MCP4725_H_
+#define MCP4725_H_
+
+#include "i2c_master.h"
+#include <stdint.h>
+
+#define WRITE_DAC    0x40
+#define WRITE_EEPROM 0x60
+
+#define MCP4725_MAX  0xFFF
+
+#define MCP4725_ADDR 0x60
+
+void mcp4725_init(uint8_t address);
+void mcp4725_write(uint16_t data);
+void mcp4725_read_dac(void);
+void mcp4725_read_eeprom(void);
+
+
+#endif /* MCP4725_H_ */
