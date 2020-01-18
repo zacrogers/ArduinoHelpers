0a90d7bad980667d337daeea89d3aec08310d93f
zacrogers <38870655+zacrogers@users.noreply.github.com>
2020-01-18 16:55:24 +1300

    Update 7_segment.hh

--- 7_segment.hh
+++ 7_segment.hh
@@ -1,30 +1,32 @@
 /*
     7 segment display driver using 74hc595 shift register
 
-n abcdefg
-0 1111110
-1 0110000
-2 1101101
-3 1111001
-4 0110011
-5 1011011 
-6 1011111
-7 1110000
-8 1111111
-9 1111011
-  */
+    n abcdefg
+    0 1111110
+    1 0110000
+    2 1101101
+    3 1111001
+    4 0110011
+    5 1011011 
+    6 1011111
+    7 1110000
+    8 1111111
+    9 1111011
+*/
 
-#ifndef 7_SEGMENT_H
-#define 7_SEGMENT_H
+#ifndef SEVEN_SEGMENT_H
+#define SEVEN_SEGMENT_H
 
 #define SEGMENTS 7
+//#define COMMON_CATHODE
+#define COMMON_ANODE
 
 const uint8_t DIGITS[10] = {0x7E, 0x30, 0x6D, 0x79, 0x33, 
                             0x5B, 0x5F, 0x70, 0xFF, 0x7B};
-class 7Segment
+class SevenSegment
 {
   public:
-    7Segment(uint8_t data, uint8_t clk, uint8_t latch)
+    SevenSegment(uint8_t data, uint8_t clk, uint8_t latch)
     {
         this->data = data;
         this->clk = clk;
@@ -50,6 +52,18 @@ class 7Segment
         {
             digitalWrite(this->clk, LOW);
             
+            #ifdef COMMON_ANODE
+            if(digit & (0x01 << segment))
+            {
+                digitalWrite(this->data, LOW);        
+            }
+            else                  
+            {
+                digitalWrite(this->data, HIGH);     
+            }
+            #endif
+            
+            #ifdef COMMON_CATHODE
             if(digit & (0x01 << segment))
             {
                 digitalWrite(this->data, HIGH);        
@@ -57,13 +71,15 @@ class 7Segment
             else                  
             {
                 digitalWrite(this->data, LOW);     
-            }
+            }            
+            
+            #endif
             
             digitalWrite(this->clk, HIGH);
         } 
         digitalWrite(this->latch, HIGH);     
     }
-  
+    
   private:
     uint8_t data;
     uint8_t clk;
