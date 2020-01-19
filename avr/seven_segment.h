d4aef21da5f4b8fc8b113186508a360bf7f32940
zacrogers <38870655+zacrogers@users.noreply.github.com>
2020-01-19 19:51:27 +1300

    Update seven_segment.h

--- avr/seven_segment.h
+++ avr/seven_segment.h
@@ -25,45 +25,43 @@
 #define NUM_LETTERS 20
 
 namespace z_lib{
+        /* 
+         *  Lower case letters are offset in ascii table by  
+         *  65 so this is used for finding letter
+        */
+        #define ASCII_OFFSET 65
 
+        static const uint8_t DIGITS[NUM_DIGITS] = {0x7E, 0x30, 0x6D, 0x79, 0x33, 
+                                                   0x5B, 0x5F, 0x70, 0xFF, 0x7B};
 
-/* 
- *  Lower case letters are offset in ascii table by  
- *  65 so this is used for finding letter
-*/
-#define ASCII_OFFSET 65
+                                        /* A     b     C     d     E     F */ 
+        static const uint8_t LETTERS[] = {0x77, 0x1F, 0x4E, 0x3D, 0x4F, 0x47,
+                                        /* G     H     i     J     L     n */
+                                          0x5E, 0x37, 0x06, 0x3C, 0x0E, 0x15,
+                                        /* o     p     q     r     S     t */
+                                          0x1D, 0x67, 0x73, 0x05, 0x5B, 0x0F,
+                                        /* U     y  */
+                                          0x1C, 0x3B};          
 
-static const uint8_t DIGITS[NUM_DIGITS] = {0x7E, 0x30, 0x6D, 0x79, 0x33, 
-                                           0x5B, 0x5F, 0x70, 0xFF, 0x7B};
-                            
-                                /* A     b     C     d     E     F */ 
-static const uint8_t LETTERS[] = {0x77, 0x1F, 0x4E, 0x3D, 0x4F, 0x47,
-                                /* G     H     i     J     L     n */
-                                  0x5E, 0x37, 0x06, 0x3C, 0x0E, 0x15,
-                                /* o     p     q     r     S     t */
-                                  0x1D, 0x67, 0x73, 0x05, 0x5B, 0x0F,
-                                /* U     y  */
-                                  0x1C, 0x3B};          
-                                            
-/* Alphabet for getting letters - upper case */
-static const char ALPHABET[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
-                                'I', 'J', 'L', 'N', 'O', 'P', 'Q', 'R', 
-                                'S', 'T', 'U', 'Y'};
+        /* Alphabet for getting letters - upper case */
+        static const char ALPHABET[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
+                                        'I', 'J', 'L', 'N', 'O', 'P', 'Q', 'R', 
+                                        'S', 'T', 'U', 'Y'};
 
-class SevenSegment
-{
-  public:
-    SevenSegment(Gpio data, Gpio clk, Gpio latch);
-    void set_num(uint8_t num);
-    void set_letter(char letter);
+        class SevenSegment
+        {
+          public:
+            SevenSegment(Gpio data, Gpio clk, Gpio latch);
+            void set_num(uint8_t num);
+            void set_letter(char letter);
 
-  private:
-    Gpio *data;
-    Gpio *clk;
-    Gpio *latch;
+          private:
+            Gpio *data;
+            Gpio *clk;
+            Gpio *latch;
 
-    uint8_t get_letter(char letter);
-};
+            uint8_t get_letter(char letter);
+        };
 
 }
 
