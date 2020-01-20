de9829aba49ffd4e85fb9faa251836620bb58e0c
zacrogers <38870655+zacrogers@users.noreply.github.com>
2020-01-21 11:12:08 +1300

    Updated seven segment display

--- avr/seven_segment.cpp
+++ avr/seven_segment.cpp
@@ -1,108 +1,104 @@
+/*
+ * seven_segment.cpp
+ *
+ * 7 segment display driver using 74hc595 shift register
+ *
+ * Created: 21-Jan-20 10:22:20 AM
+ *  Author: Zac
+ */ 
+
 #include "seven_segment.h"
-
-/* 
- *  Lower case letters are offset in ascii table by  
- *  65 so this is used for finding letter
-*/
-#define ASCII_OFFSET 65
-
-static const uint8_t DIGITS[NUM_DIGITS] = {0x7E, 0x30, 0x6D, 0x79, 0x33, 
-                                           0x5B, 0x5F, 0x70, 0xFF, 0x7B};
-                            
-                                /* A     b     C     d     E     F */ 
-static const uint8_t LETTERS[] = {0x77, 0x1F, 0x4E, 0x3D, 0x4F, 0x47,
-                                /* G     H     i     J     L     n */
-                                  0x5E, 0x37, 0x06, 0x3C, 0x0E, 0x15,
-                                /* o     p     q     r     S     t */
-                                  0x1D, 0x67, 0x73, 0x05, 0x5B, 0x0F,
-                                /* U     y  */
-                                  0x1C, 0x3B};          
-                                            
-/* Alphabet for getting letters - upper case */
-static const char ALPHABET[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
-                                'I', 'J', 'L', 'N', 'O', 'P', 'Q', 'R', 
-                                'S', 'T', 'U', 'Y'};
-
-
 
 namespace z_lib
 {
-    SevenSegment::SevenSegment(Gpio *data, Gpio *clk, Gpio *latch)
-    {
-        this->data = data;
-        this->clk = clk;
-        this->latch = latch;
-    }
-    
-    void SevenSegment::set_num(uint8_t num)
-    {
-        uint8_t digit = 0;
-        
-        if(num < NUM_DIGITS)
-        {
-            digit = DIGITS[num];
-        }
+	SevenSegment::SevenSegment(Gpio* data, Gpio* clk, Gpio* latch)
+	{
+		this->data = data;
+		this->clk = clk;
+		this->latch = latch;
+	}
+	
+	void SevenSegment::set_num(uint8_t num)
+	{
+		uint8_t digit = 0;
+		
+		if(num < NUM_DIGITS)
+		{
+			digit = DIGITS[num];
+		}
+		write_to_disp(digit);
+	}
 
-        *this->latch.clear();
+	void SevenSegment::set_letter(char letter)
+	{
+		write_to_disp(LETTERS[get_letter(letter)]);
+	}
+	
+	void SevenSegment::write_to_disp(uint8_t character)
+	{
+		this->latch->clear();
 
-        for(uint8_t segment = 0; segment < SEGMENTS; segment++)
-        {
-            *this->clk.clear();
-            
-            if(digit & (0x01 << segment))
-            {
-                *this->data.clear();
-            }
-            else                  
-            {
-                *this->data.set();
-            }
-            *this->clk.set();
-        } 
-        *this->latch.set();
-    }
+		for(uint8_t segment = 0; segment < SEGMENTS; ++segment)
+		{
+			this->clk->clear();
+			
+			if(character & (0x01 << segment))
+			{
+				this->data->clear();
+			}
+			else
+			{
+				this->data->set();
+			}
+			this->clk->set();
+		}
+		this->latch->set();
+	}
+	
+	/* Get index of letter in ALPHABET to get display pattern in LETTERS */
+	uint8_t SevenSegment::get_letter(const char letter)
+	{
+		uint8_t letter_index = 0;
+	    
+		for(uint8_t let = 0; let < NUM_LETTERS; ++let)
+		{
+			if(is_upper(&letter))
+			{
+				if(ALPHABET[let] == letter)
+				{
+					letter_index = let;
+				}
+			}
+			else
+			{
+				if(ALPHABET[let] == lower_to_upper(&letter))
+				{
+					letter_index = let;
+				}
+			}
+		}
+		return letter_index;
+	}
 
-    void SevenSegment::set_letter(char letter)
-    {
-      uint8_t curr_letter = LETTERS[get_letter(letter)];
-      
-        *this->latch.clear();
+	bool SevenSegment::is_upper(const char* letter)
+	{
+		bool ret_val = false;
+	    
+		if(letter)
+		{
+			ret_val = ((*letter < 91) ? true : false); /* Max upper case letter in ascii table is Z at 90 */
+		}
+		return ret_val;
+	}
 
-        for(uint8_t segment = 0; segment < SEGMENTS; segment++)
-        {
-            *this->latch.clear();
-            
-            if(curr_letter & (0x01 << segment))
-            {
-                *this->data.clear();
-            }
-            else                  
-            {
-                *this->data.set();
-            }
-            
-            *this->clk.set();
-        } 
-        *this->latch.set();      
-    }
-
-    uint8_t SevenSegment::get_letter(char letter);
-    {
-      uint8_t letter_index   = 0;
-      
-      for(uint8_t let = 0; let < NUM_LETTERS; ++let)
-      {
-          /* Compare upper case */
-          if(ALPHABET[let] == letter)
-          {
-             letter_index = let;
-          }
-          /* Compare lower case:*/
-          else if(ALPHABET[let] == (letter + ASCII_OFFSET))
-          {
-            letter_index = let; 
-          }             
-      }
-      return letter_index;       
-    }
+	char SevenSegment::lower_to_upper(const char* lower)
+	{
+		char upper = 0;
+	    
+		if(lower)
+		{
+			upper = (*lower - ASCII_OFFSET);
+		}
+		return upper;
+	}	
 }
--- avr/seven_segment.h
+++ avr/seven_segment.h
@@ -1,68 +1,64 @@
 /*
-    7 segment display driver using 74hc595 shift register
-
-    n abcdefg
-    0 1111110
-    1 0110000
-    2 1101101
-    3 1111001
-    4 0110011
-    5 1011011 
-    6 1011111
-    7 1110000
-    8 1111111
-    9 1111011
-*/
-
-#ifndef SEVEN_SEGMENT_H
-#define SEVEN_SEGMENT_H
+* seven_segment.h
+*
+* 7 segment display driver using 74hc595 shift register
+*
+* Created: 21-Jan-20 9:51:47 AM
+*  Author: Zac
+*/ 
+#ifndef SEVEN_SEGMENT_H_
+#define SEVEN_SEGMENT_H_
 
 #include <stdint.h>
 #include "gpio.h"
 
-#define SEGMENTS    7
-#define NUM_DIGITS  10
-#define NUM_LETTERS 20
+/* 
+*  Lower case letters are offset from upper case in ascii table by  
+*  32 so this is used for converting case
+*/
+#define ASCII_OFFSET 32
+#define SEGMENTS     7
+#define NUM_DIGITS   10
+#define NUM_LETTERS  20
 
-namespace z_lib{
-        /* 
-         *  Lower case letters are offset in ascii table by  
-         *  65 so this is used for finding letter
-        */
-        #define ASCII_OFFSET 65
+namespace z_lib
+{
+	static const uint8_t DIGITS[NUM_DIGITS] = {0x7E, 0x30, 0x6D, 0x79, 0x33, 
+											   0x5B, 0x5F, 0x70, 0xFF, 0x7B};
+                            
+											   /* A     b     C     d     E     F */ 
+	static const uint8_t LETTERS[NUM_LETTERS] = {0x77, 0x1F, 0x4E, 0x3D, 0x4F, 0x47,
+											   /* G     H     i     J     L     n */
+												 0x5E, 0x37, 0x06, 0x3C, 0x0E, 0x15,
+											   /* o     p     q     r     S     t */
+												 0x1D, 0x67, 0x73, 0x05, 0x5B, 0x0F,
+											   /* U     y  */
+												 0x1C, 0x3B};          
+                                            
+	/* Alphabet for getting letters - upper case */
+	static const char ALPHABET[NUM_LETTERS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
+											   'I', 'J', 'L', 'N', 'O', 'P', 'Q', 'R', 
+											   'S', 'T', 'U', 'Y'};
 
-        static const uint8_t DIGITS[NUM_DIGITS] = {0x7E, 0x30, 0x6D, 0x79, 0x33, 
-                                                   0x5B, 0x5F, 0x70, 0xFF, 0x7B};
+	class SevenSegment
+	{
+		public:
+			SevenSegment(Gpio* data, Gpio* clk, Gpio* latch);
 
-                                        /* A     b     C     d     E     F */ 
-        static const uint8_t LETTERS[] = {0x77, 0x1F, 0x4E, 0x3D, 0x4F, 0x47,
-                                        /* G     H     i     J     L     n */
-                                          0x5E, 0x37, 0x06, 0x3C, 0x0E, 0x15,
-                                        /* o     p     q     r     S     t */
-                                          0x1D, 0x67, 0x73, 0x05, 0x5B, 0x0F,
-                                        /* U     y  */
-                                          0x1C, 0x3B};          
+			void set_num    (uint8_t num);
+			void set_letter (char letter);
 
-        /* Alphabet for getting letters - upper case */
-        static const char ALPHABET[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
-                                        'I', 'J', 'L', 'N', 'O', 'P', 'Q', 'R', 
-                                        'S', 'T', 'U', 'Y'};
+		private:
+			Gpio* data;
+			Gpio* clk;
+			Gpio* latch;
 
-        class SevenSegment
-        {
-          public:
-            SevenSegment(Gpio data, Gpio clk, Gpio latch);
-            void set_num(uint8_t num);
-            void set_letter(char letter);
+			void           write_to_disp  (uint8_t character);  /* Write character to display */
+			static uint8_t get_letter     (const char letter);  /* Get index of letter in ALPHABET */
+			static bool    is_upper       (const char* letter); /* Check case of ascii char */
+			static char    lower_to_upper (const char* lower);  /* Convert case of ascii char */
 
-          private:
-            Gpio *data;
-            Gpio *clk;
-            Gpio *latch;
-
-            uint8_t get_letter(char letter);
-        };
-
+	};
 }
 
-#endif /* 7_SEGMENT_H */
+#endif /* SEVEN_SEGMENT_H_ */
